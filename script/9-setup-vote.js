de4b01d331b565805ad29e2bf9f19cbdb523a99d
simonbusch <Simon.busch@live.fr>
2022-04-02 11:16:55 +0200

    add set up vote

---
+++ script/9-setup-vote.js
@@ -0,0 +1,45 @@
+import sdk from "./1-SDK.js";
+
+// This is our governance contract.
+const vote = sdk.getVote("0x61A0cE85D8675057daACAD63BF2b5BE21fBB329C");
+
+// This is our ERC-20 contract.
+const token = sdk.getToken("0x8Ee82768B7C460A494162D5e0F40740d23a358Fe");
+
+(async () => {
+  try {
+    // Give our treasury the power to mint additional token if needed.
+    await token.roles.grant("minter", vote.getAddress());
+
+    console.log(
+      "Successfully gave vote contract permissions to act on token contract"
+    );
+  } catch (error) {
+    console.error(
+      "failed to grant vote contract permissions on token contract",
+      error
+    );
+    process.exit(1);
+  }
+
+  try {
+    // Grab our wallet's token balance, remember -- we hold basically the entire supply right now!
+    const ownedTokenBalance = await token.balanceOf(
+      process.env.WALLET_ADDRESS
+    );
+
+    // Grab 90% of the supply that we hold.
+    const ownedAmount = ownedTokenBalance.displayValue;
+    const percent90 = Number(ownedAmount) / 100 * 90;
+
+    // Transfer 90% of the supply to our voting contract.
+    await token.transfer(
+      vote.getAddress(),
+      percent90
+    ); 
+
+    console.log("✅ Successfully transferred " + percent90 + " tokens to vote contract");
+  } catch (err) {
+    console.error("failed to transfer tokens to vote contract", err);
+  }
+})();
